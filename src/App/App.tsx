0ea6fc897ae0fe4bcc7ceb42d2c931dbe99392c5
ArhipSizov <arhip.sizov111@gmail.com>
2025-06-09 20:13:33 +0300

    change ability

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,39 +1,53 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
+interface Ability {
+  id: string;
+  name: string;
+  damage: number;
+  description: string;
+}
+
 interface Character {
   id: string;
   name: string;
   hp: number;
   maxHp: number;
   position: number;
-  // skills: Skill[];
+  skills: [Ability, Ability, Ability];
 }
 
-interface Ability {
-  id: string;
-  name: string;
-  damage: number;
-  // skills: Skill[];
-}
 function App() {
   const [turn, setTurn] = useState<number>(0);
 
-  //declare ability
-  const [Firebol] = useState<Ability>({
+  //declare ally
+  const [Mage] = useState<Character>({
     id: "1",
-    name: "Firebol",
-    damage: 4,
-  });
-  const [Kick] = useState<Ability>({
-    id: "2",
-    name: "Kick",
-    damage: 2,
-  });
-  const [Fire] = useState<Ability>({
-    id: "3",
-    name: "Fire",
-    damage: 1,
+    name: "маг",
+    hp: 20,
+    maxHp: 20,
+    position: 1,
+    skills: [
+      {
+        id: "1",
+        name: "Firebol",
+        damage: 4,
+        description:
+          "very long textvery long textvery long textvery long textvery long textvery long text",
+      },
+      {
+        id: "2",
+        name: "Kick",
+        damage: 2,
+        description: "ewrewrew",
+      },
+      {
+        id: "3",
+        name: "Fire",
+        damage: 1,
+        description: "fdshhgkjjg",
+      },
+    ],
   });
 
   //declare enemy
@@ -43,6 +57,26 @@ function App() {
     hp: 12,
     maxHp: 12,
     position: 1,
+    skills: [
+      {
+        id: "1",
+        name: "nothin",
+        damage: 0,
+        description: "none",
+      },
+      {
+        id: "2",
+        name: "nothin",
+        damage: 0,
+        description: "none",
+      },
+      {
+        id: "3",
+        name: "nothin",
+        damage: 0,
+        description: "none",
+      },
+    ],
   });
   const [Goblin1] = useState<Character>({
     id: "2",
@@ -50,6 +84,26 @@ function App() {
     hp: 12,
     maxHp: 12,
     position: 1,
+    skills: [
+      {
+        id: "1",
+        name: "nothin",
+        damage: 0,
+        description: "none",
+      },
+      {
+        id: "2",
+        name: "nothin",
+        damage: 0,
+        description: "none",
+      },
+      {
+        id: "3",
+        name: "nothin",
+        damage: 0,
+        description: "none",
+      },
+    ],
   });
   const [Goblin2] = useState<Character>({
     id: "3",
@@ -57,6 +111,26 @@ function App() {
     hp: 12,
     maxHp: 12,
     position: 1,
+    skills: [
+      {
+        id: "1",
+        name: "nothin",
+        damage: 0,
+        description: "none",
+      },
+      {
+        id: "2",
+        name: "nothin",
+        damage: 0,
+        description: "none",
+      },
+      {
+        id: "3",
+        name: "nothin",
+        damage: 0,
+        description: "none",
+      },
+    ],
   });
   const [Goblin3] = useState<Character>({
     id: "4",
@@ -64,9 +138,29 @@ function App() {
     hp: 12,
     maxHp: 12,
     position: 1,
+    skills: [
+      {
+        id: "1",
+        name: "nothin",
+        damage: 0,
+        description: "none",
+      },
+      {
+        id: "2",
+        name: "nothin",
+        damage: 0,
+        description: "none",
+      },
+      {
+        id: "3",
+        name: "nothin",
+        damage: 0,
+        description: "none",
+      },
+    ],
   });
   console.log(Goblin0, Goblin1, Goblin2, Goblin3);
-  
+
   //enemy active
   const [enemy, setEnemy] = useState<string[]>([
     "1",
@@ -109,7 +203,6 @@ function App() {
   //atack
   function atack() {
     setTurn(turn + 1);
-    const arrAbility = [Firebol, Kick, Fire];
     let chooseEnemy;
     let damage;
 
@@ -120,9 +213,9 @@ function App() {
       }
     }
 
-    arrAbility.forEach((elem) => {
+    Mage.skills.forEach((elem) => {
       if (ability[0] == elem.id) {
-        damage = elem.damage;
+        damage = Mage.skills[Number(ability[0]) - 1].damage;
       }
     });
 
@@ -153,7 +246,6 @@ function App() {
     if (idEnemy) {
       idEnemy.style.width = width + "%";
     }
-    console.log(1);
   }, [turn]);
 
   //site
@@ -200,7 +292,18 @@ function App() {
             className={ability[3]}
           ></div>
         </div>
-        <div onClick={() => atack()} className="and_turn"></div>
+        <div className="ability_description">
+          <p className="name">{Mage.skills[Number(ability[0]) - 1].name}</p>
+          <p className="description">
+            {Mage.skills[Number(ability[0]) - 1].description}
+          </p>
+          <p className="damage">
+            Урон - {Mage.skills[Number(ability[0]) - 1].damage}
+          </p>
+        </div>
+        <div onClick={() => atack()} className="and_turn">
+          <p>Завершить ход</p>
+        </div>
       </div>
     </div>
   );
